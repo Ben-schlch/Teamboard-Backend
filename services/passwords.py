b3e644cf51722542ec2e86c11de8d90d71f69193
alwin <alwin@zomotor.de>
2023-03-23 10:40:26 +0100

    added email-dummyfile + salt and hash function

--- services/passwords.py
+++ services/passwords.py
@@ -1,7 +1,8 @@
 import bcrypt
+from typing import Tuple
 
 
-def hash_and_salt(pw: str):
+def hash_and_salt(pw: str) -> Tuple[bytes, bytes]:
     salt = bcrypt.gensalt(15)
     hashed_pw = bcrypt.hashpw(pw.encode("utf-8"), salt)
     return salt, hashed_pw
--- services/register.py
+++ services/register.py
@@ -1,12 +1,15 @@
 from services.dbconnection import connect
 from services.passwords import hash_and_salt, check_pw
+from services.emails import send_email
 
 
 def register_user(name, email, pwd, profilepicture=None):
+    salt, pwd = hash_and_salt(pwd)
     conn, cur = connect()
-    sql = f"INSERT INTO users (username, mail, pwd, verified, profilepicture) " \
-          f"VALUES ('{name}', '{email}', '{pwd}', 'false', '{profilepicture}')"
+    sql = f"INSERT INTO users (username, mail, pwd, salt, verified, profilepicture) " \
+          f"VALUES ('{name}', '{email}', '{pwd}', '{salt}', 'false', '{profilepicture}')"
     cur.execute(sql)
-    resp = cur.fetchall()
-    print(resp)
+    msg = f"""Hello {name},
+    Thank you for signing up for our service. please confirm your email-adress with this link:"""
+    send_email(email, "Confirm your teamboard-adress", msg)
 
