3bd4e0a4559420f59d81102dbeee83e4f565da3e
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2023-03-27 15:50:04 +0200

    ip test

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ html = """
         <script>
             var client_id = Date.now()
             document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://${window.location.href}:8000/ws/${client_id}`);
+            var ws = new WebSocket(`ws://http://195.201.94.44/:8000/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
