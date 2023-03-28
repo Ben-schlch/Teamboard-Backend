d31cf38504414d7e3b8373265d9857250b0b2d1b
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2023-03-28 10:49:30 +0200

    included jwt tokens //not tested but needs merg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-
+from fastapi import FastAPI, WebSocket, Depends, HTTPException, status, WebSocketDisconnect
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
 import logging
 from datetime import datetime
 
@@ -10,10 +11,46 @@ logger = logging.getLogger("websocket fastapi")
 app = FastAPI()
 
 
+# Secret key used to sign JWT tokens
+SECRET_KEY = "mysecretkey"
+# Time in minutes before JWT token expires
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# Security scheme to handle JWT tokens
+security = HTTPBearer()
+
+# Function to generate JWT token
+def generate_token(email: str, password: str):
+    #TODO CHeck login:
+    #login_user(email,pw)
+    if user:
+        expiration_time = time.time() + (ACCESS_TOKEN_EXPIRE_MINUTES * 60)
+        token = jwt.encode({"sub": user["email"], "exp": expiration_time}, SECRET_KEY, algorithm=ALGORITHM)
+        return token
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
+
 async def parse_message(data: dict):
     """Check the message from client"""
     #TODO: JSON Format -->
     return 0
+
+def verify_token(token: str):
+    """
+    erify a JWT token and return its payload
+    @param token:
+    @return:
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload["sub"]
+        expiration_time = payload.get("exp", None)
+        if expiration_time is not None and time.time() > expiration_time:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+        return email
+    except jwt.exceptions.DecodeError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    except jwt.exceptions.InvalidSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token signature")
 
 
 async def send_personal_message(message: str, websocket: WebSocket):
@@ -39,21 +76,29 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+# Define a WebSocket endpoint that requires JWT token authentication
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, token: str):
+    email = verify_token(token)
 
-@app.websocket("/ws/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: str):
-    # TODO: Put websocket connection on pending
-    # TODO: Check first message whether it is authenticated --> Put
-    #  websocket into list of authenticated for 15 minutes (database??!)
-    #  --> how does authentication work? alwin please explain --> Cookie?
-
+    # Handle WebSocket connection
     await manager.connect(websocket)
-
+    await websocket.send_text(f"Welcome, {email}!")
     try:
         while True:
             data = await websocket.receive_text()
             await send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{user_id} says: {data}")
+            await manager.broadcast(f"Client #{email} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{user_id} left the chat")
+        await manager.broadcast(f"Client #{email} left the chat")
+
+# Define an HTTP endpoint that generates a JWT token given an email and password
+@app.post("/token")
+def get_token(email: str, password: str):
+    return {"token": generate_token(email, password)}
+
+
+
+
+
