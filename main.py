d12f5a36cd0e56c80025e75731e43a64cf046d16
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2023-03-29 19:50:38 +0200

    updated users and outsource connectionmanager.py

--- main.py
+++ main.py
@@ -1,89 +1,17 @@
-from pydantic import BaseModel
-from services.users import register_user, login_user
-from fastapi import FastAPI, WebSocket, status, WebSocketDisconnect, HTTPException
-import jwt
+from services.users import register_user, UserBody, Credentials
+from services.connectionmanager import ConnectionManager, verify_token, generate_token
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
 import logging
-import time
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
-
-# Secret key used to sign JWT tokens
-SECRET_KEY = "mysecretkey"
-# Time in minutes before JWT token expires
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-# Algorithm to generate the jwt token
-ALGORITHM = "HS256"
-
-
-class UserBody(BaseModel):
-    name: str
-    email: str
-    pwd: str
-
-
-class Credentials(BaseModel):
-    email: str
-    pwd: str
-
-
-# Function to generate JWT token
-async def generate_token(email: str, pwd: str):
-    if await login_user(email, pwd):
-        expiration_time = time.time() + (ACCESS_TOKEN_EXPIRE_MINUTES * 60)
-        token = jwt.encode({"sub": email, "exp": expiration_time}, SECRET_KEY, algorithm=ALGORITHM)
-        return token
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
 
 
 async def parse_message(data: dict):
     """Check the message from client"""
     # TODO: JSON Format -->
     return 0
-
-
-def verify_token(token: str):
-    """
-    verify a JWT token and return its payload
-    @param token:
-    @return:
-    """
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        email = payload["sub"]
-        expiration_time = payload.get("exp", None)
-        if expiration_time is not None and time.time() > expiration_time:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
-        return email
-    except jwt.exceptions.InvalidSignatureError as e:
-        print(e)
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token signature")
-    except jwt.exceptions.DecodeError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-
-
-async def send_personal_message(message: str, websocket: WebSocket):
-    # FIXME: Function or static method in Connection manager?
-    await websocket.send_text(message)
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
 
 manager = ConnectionManager()
 
@@ -105,7 +33,7 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             while True:
                 data = await websocket.receive_text()
-                await send_personal_message(f"You wrote: {data}", websocket)
+                await manager.send_personal_message(f"You wrote: {data}", websocket)
                 await manager.broadcast(f"Client #{email} says: {data}")
         except WebSocketDisconnect:
             manager.disconnect(websocket)
@@ -126,7 +54,6 @@ async def root():
 @app.post("/register/")
 async def register(user: UserBody):
     return await register_user(**user.dict())
-
 
 # # debug:
 # import uvicorn
--- services/users.py
+++ services/users.py
@@ -1,11 +1,22 @@
 from services.db import insert_query, select_query
 from services.passwords import hash_and_salt, check_pw
 from services.emails import send_email
-import psycopg2
 from fastapi import HTTPException
+from pydantic import BaseModel
 
 
-async def register_user(name, email, pwd, profilepicture=None):
+class UserBody(BaseModel):
+    name: str
+    email: str
+    pwd: str
+
+
+class Credentials(BaseModel):
+    email: str
+    pwd: str
+
+
+async def register_user(name, email, pwd):
     salt, pwd = hash_and_salt(pwd)
     res = insert_query("users",
                        ['username', 'mail', 'pwd', 'salt'],
@@ -24,7 +35,7 @@ async def login_user(email: str, pwd: str):
     # There will be only one result because the mail attribute is the PRIMARY KEY
     try:
         res = select_query("users", ["pwd"], f"mail = '{email}'")[0]
-    except IndexError as e:
+    except IndexError:
         return False
     if type(res) is int:
         return False
