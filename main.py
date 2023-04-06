70c2ecffa7031b6e54d8d022641cce131d0909b8
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2023-04-06 12:44:50 +0200

    more precise errors

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def websocket_endpoint(websocket: WebSocket, token: str):
                     await manager.send_personal_message(f"400 JSONDecodeError", websocket)
                 except Exception as e:
                     logging.info(f"400 Error with {websocket}: {str(e)}")
-                    await manager.send_personal_message(f"400 Error", websocket)
+                    await manager.send_personal_message(f"400 Error Parsemessage", websocket)
                 # await manager.send_personal_message(f"You wrote: {data}", websocket)
                 # await manager.broadcast(f"Client #{email} says: {data}")
         except WebSocketDisconnect:
