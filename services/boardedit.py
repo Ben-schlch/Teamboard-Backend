a072fbedcb43fe051e593436a5f4392266c90a9a
Ben-schlch <inf21140@lehre.dhbw-suttgart.de>
2023-04-06 15:03:48 +0200

    fixed date empty string problems

--- services/boardedit.py
+++ services/boardedit.py
@@ -213,7 +213,7 @@ async def subtaskcreate(data):
               "(part_of_teamboard, part_of_task, part_of_column, " \
               "subtask_name, deadline, color, description, worker, l_neighbor) " \
               "VALUES (%s, %s, %s, %s, nullif(%s,''), %s, %s, %s, %s ) RETURNING subtask_id;"
-        values = (data["teamboard_id"], data["task_id"], data["state_id"], max_columns["name"], max_columns["created"],
+        values = (data["teamboard_id"], data["task_id"], data["state_id"], max_columns["name"],
                   max_columns["deadline"],
                   max_columns["color"], max_columns["description"], max_columns["worker"], l_neighbor)
         cur.execute(sql, values)
