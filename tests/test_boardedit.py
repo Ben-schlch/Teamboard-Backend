21b576cfad32d6e714121dd01ee9f3aebcfbdabd
Alwin Zomotor <inf21034@lehre.dhbw-stuttgart.de>
2023-04-22 10:54:46 +0200

    added moving subtasks between states and added testdata resource directory

--- tests/test_boardedit.py
+++ tests/test_boardedit.py
@@ -1,5 +1,5 @@
 from unittest import IsolatedAsyncioTestCase
-from services.boardedit import teamboardload, teamboardcreate
+from services.boardedit import teamboardload, teamboardcreate, subtaskmove
 import json
 
 
@@ -11,4 +11,9 @@ class Test(IsolatedAsyncioTestCase):
     async def test_teamboardcreate(self):
         res = await teamboardcreate(json.loads('{"kind_of_object": "board", "type_of_edit": "add", "teamboard": {"id": 24, "name": "oof", "tasks": []}}'), "inf21034@lehre.dhbw-stuttgart.de")
         print(res)
+
+    async def test_move_subtask(self):
+        with open('./testdata/subtask-between-states.json') as f:
+            res = await subtaskmove(json.load(f))
+            print(res)
 
