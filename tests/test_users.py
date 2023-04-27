c5d16823a9a2191990ac056e902a1b4335b8acd7
Alwin Zomotor <inf21034@lehre.dhbw-stuttgart.de>
2023-04-27 12:20:33 +0200

    changed email

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,6 +4,9 @@ from services.users import login_user, register_user
 
 class Test(IsolatedAsyncioTestCase):
     async def test_register_user(self):
-        res = await register_user("test", "ben+schlauch@gmail.com", "test")
+        res = await register_user("test", "teamboard.projekt@gmail.com", "Hallo1234")
+
+
     async def test_login_user(self):
-        res = login_user("ben.schlauch+++@gmail.com", "test")
+        res = await login_user("teamboard.projekt@gmail.com", "Hallo1234")
+        print(res)
